f046508b6d5914b154b0c49b38e188b9f17b5e7a
Nipun Madhawa <nipun.madhawa96@gmail.com>
2020-12-21 21:03:33 +0530

    use useState

--- src/features/signup/Login.jsx
+++ src/features/signup/Login.jsx
@@ -3,17 +3,23 @@ import React, { useState } from 'react'
 export default function Login() {
     const [statement, setstatement] = useState("");
     const [visibility, setvisibility] = useState(false);
+    const [username1, setusername1] = useState("");
+    const [password1, setpassword1] = useState("");
     return (
         <div>
             <div style={{paddingLeft:'40%'}}>
-                <input  id='username1' placeholder={"UserName"}></input><br></br>
-                <input id='password1' placeholder={"Password"}></input><br></br>
+                <input  id='username1' placeholder={"UserName"} value={username1} onChange={(e) => {
+                    setusername1(e.target.value)
+                }}></input><br></br>
+                <input id='password1' placeholder={"Password"} value={password1} onChange={(e) => {
+                    setpassword1(e.target.value)
+                }}></input><br></br>
 
                 <div style={{paddingLeft:'8%'}}>
-                    <button  onClick={()=> {localStorage.getItem('username')===document.getElementById("username1").value ? (
-                    localStorage.getItem('password')===document.getElementById("password1").value ? (
+                    <button  onClick={()=> {localStorage.getItem('username')===username1 ? (
+                    localStorage.getItem('password')===password1 ? (
                     setstatement("log in success") ):setstatement("Pasword and Username doesn't match")) :
-                    setstatement("Login faild");setvisibility(true) ;document.getElementById("username1").value=''; document.getElementById("password1").value=''}}>Log In</button>
+                    setstatement("Login faild");setvisibility(true) ;setusername1(''); setpassword1('')}}>Log In</button>
                 </div>
                 <div>
                     {visibility ? <p value={statement} >{statement}</p> : null}
--- src/features/signup/Signup.jsx
+++ src/features/signup/Signup.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Signup() {
-    
+    const [username, setusername] = useState("")
+    const [password, setpassword] = useState("")
+
+
     return (
         <div>
             <div style={{paddingLeft:'40%'}}>
-            <input  id='username' placeholder={"UserName"}></input><br></br>
-            <input id='password' placeholder={"Password"}></input><br></br>
+            <input  id='username' placeholder={"UserName"} value={username} onChange={(e) => {
+                setusername(e.target.value)
+            }}></input><br></br>
+            <input id='password' placeholder={"Password"} value={password} onChange={(e) => {
+                setpassword(e.target.value)
+            }}></input><br></br>
             <div style={{paddingLeft:'8%'}}>
-            <button  onClick={()=> {localStorage.setItem('username',document.getElementById("username").value);
-                                    localStorage.setItem('password',document.getElementById("password").value);
-                                    document.getElementById("username").value='';
-                                    document.getElementById("password").value='';}}>Sign Up</button>
+            <button  onClick={()=> {localStorage.setItem('username',username);
+                                    localStorage.setItem('password',password);
+                                    setusername('');
+                                    setpassword('');}}>Sign Up</button>
             </div>
             </div>                        
         </div>
