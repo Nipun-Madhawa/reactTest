ffd4518cc040a5f92f7c1744620d762cc68afc63
Nipun Madhawa <nipun.madhawa96@gmail.com>
2020-12-26 21:19:36 +0530

    Added react router

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,55 @@ import './App.css';
 import Counter from './features/Counter';
 import Signup from './features/signup/Signup';
 import Login from './features/signup/Login';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container';
 
 
 function App() {
   console.log(localStorage.getItem('username'));
   console.log(localStorage.getItem('password'));
   return (
-    <div >
+    <div>
+    <Router>
+      <div>
+      <Container>
+      <Nav variant="pills" defaultActiveKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/">LogIn</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/signup">SignUp</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/counter" >
+          Counter
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      </Container>
       <br></br>
-      
-      <Counter></Counter>
-          
-      <br></br>
-
-      <Signup></Signup>
-
-      <br></br>
-
-
-      <Login></Login>
-      <br></br>
-
+        
+        <Switch>
+          <Route path="/counter">
+            <Counter />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     </div>
+    
   );
 }
 
